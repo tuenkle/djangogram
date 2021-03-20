4417a1f6cf41eee253ce6ccc961b5213988ca7be
tuenkle <tuenkle1011@gmail.com>
2021-03-21 08:53:11 +0900

    second commit 21.03.21

--- djangogram/users/forms.py
+++ djangogram/users/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import forms as admin_forms
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
-
+from django import forms as django_form
 User = get_user_model()
 
 
@@ -17,3 +17,23 @@ class UserCreationForm(admin_forms.UserCreationForm):
         error_messages = {
             "username": {"unique": _("This username has already been taken.")}
         }
+class SignUpForm(django_form.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'name', 'username', 'password']
+        labels = {
+            'email': '이메일 주소',
+            'name': '성명',
+            'username': '사용자 이름',
+            'password': '비밀번호',
+        }
+
+        widgets = {
+            'password': django_form.PasswordInput(),
+        }
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
